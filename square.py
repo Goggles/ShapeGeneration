b6cc66efc461dda95d65940ec7f1aee36a9ee2b2
Yarrow Paddon-Butler <yarrowpb@gmail.com>
2013-04-01 22:19:52 +0100

    added a comment to help explain the rectangle method

--- square.py
+++ square.py
@@ -1,10 +1,10 @@
 from PIL import Image, ImageDraw
 
-size = (100, 100)
+size = (100, 100) 
 img = Image.new('RGB', size)
 draw = ImageDraw.Draw(img)
 
+#the first four parameters are the points of the rectangle
 draw.rectangle((10, 10, 90, 90), fill="yellow", outline="red")
-
 
 img.save("square.png")
